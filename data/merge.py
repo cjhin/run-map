14e60a2b1d05775cbb775a2cd18c278b32606a0a
cjhin <cjhin@civisanalytics.com>
2015-04-11 19:58:35 -0500

    update the merge script, also the data file

--- data/merge.py
+++ data/merge.py
@@ -1,28 +1,33 @@
 #!/usr/bin/python
 
-import os, sys
+# Take all .gpx files in this directory
+# and merge them into one large file
+
+import os
 
 path = "./"
 dirs = os.listdir( path )
 
 output = open('all.gpx', 'w')
 
+# header for the gpx xml file
 output.write('<?xml version="1.0" encoding="UTF-8"?>')
 output.write('<gpx>')
 
-# This would print all the files and directories
 for file in dirs:
-    f = open(file, 'r')
+    if file != 'all.gpx':
+        f = open(file, 'r')
 
-    while True:
-        line = f.readline()
-        if line.startswith('<trkpt'):
-            output.write(line)
+        while True:
+            line = f.readline()
+            # only write the <trkpt tags
+            if line.startswith('<trkpt'):
+                output.write(line)
 
-        # end of file
-        if len(line) < 2:
-            break
-    f.close()
+            # end of file
+            if len(line) < 2:
+                break
+        f.close()
 
 output.write('</gpx>')
 output.close()
